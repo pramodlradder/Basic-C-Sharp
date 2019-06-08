9de5d9b0b78ba12b8196f45af848a64c9967a62e
Pramod <pramodradder123@gmail.com>
2019-06-08 18:57:57 +0530

    Simple Vehical class example

--- BasicCSharp/Program.cs
+++ BasicCSharp/Program.cs
@@ -6,48 +6,54 @@ using System.Threading.Tasks;
 
 namespace BasicCSharp
 {
-    class Program
+    class Vehical
     {
+        String Name;
+        Int32 NWheels;
+        Int32 Price;
+        String Company;
+        public Vehical(String Name)
+        {
+            this.Name = Name;
+        }
+        
+        public void setattribute()
+        {
+            Console.WriteLine("Enter the number of Wheels for :"+this.Name);
+            NWheels = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the  Price for :" + this.Name);
+            Price = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Company for :" + this.Name);
+            Company = Console.ReadLine();
+
+        }
+        public string getName()
+        {
+            return Name;
+        }
+        public Int32 getNWheels()
+        {
+            return NWheels;
+        }
+        public Int32 getPrice()
+        {
+            return Price;
+        }
+        public string getCompany()
+        {
+            return Company;
+        }
+        public void display()
+        {
+            Console.WriteLine("This "+getName() + " Has " + getNWheels() + " Wheels with price of "+getPrice()+" from a Company "+getCompany()+".");
+        }
         static void Main(string[] args)
         {
-            Int32 a = 10;
-            if(a==10)
-            {
-                Console.WriteLine("a is 10");
-            }
-            else
-            {
-                Console.WriteLine("a is not 10");
-            }
-            for(Int32 i = 0; i < a; i++)
-            {
-                Console.WriteLine("i is {0}",i);
-            }
-            //read from the user
-            Console.WriteLine("Enter the value");
-            Int32 input = Int32.Parse(Console.ReadLine());
-            while(input < 10)
-            {
-                Console.WriteLine("value is {0}",input);
-                input++;
-            }
-            Console.WriteLine("Enter the value");
-            Int32 num = Int32.Parse(Console.ReadLine());
-            switch (num)
-            {
-                case 0:
-                    Console.WriteLine("Case 0");
-                    break;
-                case 1:
-                    Console.WriteLine("Case 1");
-                    break;
-                case 2:
-                    Console.WriteLine("Case 2");
-                    break;
-                default:
-                    Console.WriteLine("Wrong Entry");
-                    break;
-            }
+            Console.WriteLine("Enter the name of Vehical");
+            String VName = Console.ReadLine();
+            Vehical v = new Vehical(VName);
+            v.setattribute();
+            v.display();
             Console.ReadKey();
         }
     }
